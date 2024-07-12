4f500eee031d1007218d344592eb857a602e94a1
chuluq <chuluq@arvis.id>
2024-07-12 11:06:57 +0700

    feat: create todo

--- todolist-service.mjs
+++ todolist-service.mjs
@@ -18,4 +18,14 @@ export class TodolistServices {
     response.write(this.getJsonTodoList());
     response.end();
   }
+
+  createTodo(request, response) {
+    request.addListener("data", (data) => {
+      const body = JSON.parse(data.toString());
+      this.todolist.push(body.todo);
+
+      response.write(this.getJsonTodoList());
+      response.end();
+    });
+  }
 }
